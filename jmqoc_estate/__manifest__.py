b3615bd5215a1a49cd0ef206f79fad40461c2ce8
Jimmy McCann <jmccann@qocinnovations.com>
2025-09-24 13:18:02 -0500

    chapter-02-a-new-application

---
+++ jmqoc_estate/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Real Estate",
+    "version": "0.0",
+    "author": "Jimmy McCann",
+    "depends": ["base"],
+    "application": True,  # so that the module appears when the ‘Apps’ filter is on
+}
