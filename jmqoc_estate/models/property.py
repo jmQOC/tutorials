5590000af22aa189b94dc2fe2a19239d806f7a63
Jimmy McCann <jmccann@qocinnovations.com>
2025-10-02 11:27:44 -0500

    chapter-08-computed-fields-and-onchanges

--- jmqoc_estate/models/property.py
+++ jmqoc_estate/models/property.py
@@ -1,5 +1,10 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.tools import date_utils
+
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 class Property(models.Model):
@@ -79,12 +84,32 @@ class Property(models.Model):
 
     garden = fields.Boolean("Has Garden")
 
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        # because of the onchange decorator, self now represents the current record in the form view
+        _logger.info(f"Garden status for {self.name} changed to {self.garden}")
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+        else:
+            self.garden_area = None
+            self.garden_orientation = None
+
     garden_area = fields.Integer("Garden Area (sq ft)")
 
     garden_orientation = fields.Selection(
         [("north", "North"), ("east", "East"), ("south", "South"), ("west", "West")],
         string="Garden Orientation",
     )
+
+    total_area = fields.Integer("Total Area (sq ft)", compute="_total_area")
+
+    @api.depends("living_area", "garden_area")
+    def _total_area(self):
+        # self is a collection, a recordset, of all the records for this particular model
+        for property in self:
+            _logger.info(f"Computing total area for property '{property.name}'")
+            property.total_area = property.living_area + property.garden_area
 
     date_available = fields.Date(
         "Date Available",
--- jmqoc_estate/models/property_offer.py
+++ jmqoc_estate/models/property_offer.py
@@ -1,4 +1,9 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.tools import date_utils
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class PropertyOffer(models.Model):
@@ -11,6 +16,31 @@ class PropertyOffer(models.Model):
     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
 
     price = fields.Float("Offer Amount")
+
+    validity = fields.Integer("Valid for (days)", default=7)
+
+    deadline = fields.Date("Decision Due", compute="_deadline", inverse="_validity")
+    # the inverse method is called when saving the record, while the compute method is called at each change of its dependencies.
+
+    @api.depends("create_date", "validity")
+    def _deadline(self):
+        # self is a collection, a recordset, of all the records for this particular model
+        for offer in self:
+            offer_create_date = offer.create_date or fields.Date.today()
+            _logger.info(
+                f"Fetched create date of {offer_create_date} for offer on {offer.property_id.name} from {offer.partner_id.name} for calculating deadline"
+            )
+            offer.deadline = date_utils.add(offer_create_date, days=offer.validity)
+
+    def _validity(self):
+        for offer in self:
+            offer_create_date = (
+                offer.create_date.date() if offer.create_date else fields.Date.today()
+            )
+            _logger.info(
+                f"Fetched create date of {offer_create_date} for offer on {offer.property_id.name} from {offer.partner_id.name} for calculating validity"
+            )
+            offer.validity = (offer.deadline - offer_create_date).days
 
     status = fields.Selection(
         [("accepted", "Accepted"), ("refused", "Refused")], copy=False
